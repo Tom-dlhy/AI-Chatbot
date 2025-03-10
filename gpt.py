a8713e8f440cce71b75227a77ff16a465a8a467a
Tom <126344245+Tom-dlhy@users.noreply.github.com>
2025-03-10 14:52:01 +0100

    v4 entrainé sur un dataset fait pour du "Customer Support" en anglais

--- gpt.py
+++ gpt.py
@@ -6,8 +6,8 @@ from torch.nn import functional as F
 batch_size = 64 # how many independent sequences will we process in parallel?
 block_size = 256 # what is the maximum context length for predictions?
 max_iters = 20000
-eval_interval = 200
-learning_rate = 3e-4
+eval_interval = 500
+learning_rate = 1e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(torch.cuda.get_device_name(0))
 
@@ -15,12 +15,11 @@ eval_iters = 200
 n_embd = 384
 n_head = 6
 n_layer = 6
-dropout = 0.5
+dropout = 0.2
 # ------------
 
 torch.manual_seed(1337)
 
-# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
